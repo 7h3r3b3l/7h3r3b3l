bada4b8cfa16454b302c39b559463523bdc94de6
7h3r3b3l <7h3r3b3l@gmail.com>
2023-12-14 02:01:48 -0500

    first commit

---
+++ scripts/update_music.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Function for creating a playlist
+update_playlist() {
+	name=$1 # Name of the playlist
+	url=$2 # Url of the youtube playlist
+	mkdir -p "$HOME/Music/$name"  && echo ''
+	# Download the playlist
+	yt-dlp $url -x --audio-format mp3 --download-archive $HOME/Music/$name/songs.txt -o "$HOME/Music/$name/%(title)s.%(ext)s" --postprocessor-args "-map_metadata -1 -vn -metadata Album='$name' -metadata album='$name' -metadata artist='---'" 
+	echo "done"
+}
+
+
+# Playlists ...
+update_playlist "Hack" "https://www.youtube.com/playlist?list=PLd1oSjQxEWgW6qCUj9u5HL7491J8tlESC"
+update_playlist "DeepHack" "https://www.youtube.com/playlist?list=PLd1oSjQxEWgU8h3MNm7kPlHbs47DUzYw_"
+update_playlist "Musica_Toda" "https://www.youtube.com/playlist?list=PLd1oSjQxEWgVmX3gZLXH5bRfglWuldzdW"
+update_playlist "Musica_Toda" "https://www.youtube.com/playlist?list=PLk9fj9cXf62kZ7oi3IzMSCnltc0S1u9LT"
+update_playlist "Musica_Toda" "https://www.youtube.com/playlist?list=PLk9fj9cXf62n4lyQ2ODCC9nP-rOTTXWvw"

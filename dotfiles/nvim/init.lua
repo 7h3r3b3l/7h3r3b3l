160752d197b0b09e4ae4741274d516fe55913268
7h3r3b3l <7h3r3b3l@proton.me>
2024-04-05 02:55:53 -0500

    autocompletion stuff done

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -44,8 +44,64 @@ local plugins= {
     -- Auto Complete quotes
 {'windwp/nvim-autopairs',event = "InsertEnter",config = true,},
     -- Floating Terminal
-{'voldikss/vim-floaterm'}
+{'voldikss/vim-floaterm'},
+    -- Autocompletion
+{
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"saadparwaiz1/cmp_luasnip",
+		"L3MON4D3/LuaSnip",
+	},
+    config = function()
+	local cmp = require("cmp")
+	vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+			end,
+		},
+		window = {
+			-- completion = cmp.config.window.bordered(),
+			-- documentation = cmp.config.window.bordered(),
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-Space>"] = cmp.mapping.complete(),
+			["<C-e>"] = cmp.mapping.abort(),
+			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		}),
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" },
+			{ name = "nvim_lua" },
+			{ name = "luasnip" }, -- For luasnip users.
+			-- { name = "orgmode" },
+		}, {
+			{ name = "buffer" },
+			{ name = "path" },
+		}),
+	})
+
+	cmp.setup.cmdline(":", {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = cmp.config.sources({
+			{ name = "path" },
+		}, {
+			{ name = "cmdline" },
+		}),
+	})
+    end
+},
+-- NEXT PLUGIN HERE
 }
+
 local opts = {}
 
 
@@ -73,6 +129,7 @@ require("catppuccin").setup {
     transparent_background = true,
     }
 }
+
 -- ELECTION OF THE THEME, CAN BE FRAPPE, MOCHA, LATE OR MACCHIATO
 vim.cmd.colorscheme "catppuccin-frappe"
 

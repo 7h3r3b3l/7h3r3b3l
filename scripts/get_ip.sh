fef89275766417a65a66c00cac8991e0b2869113
7h3r3b3l <7h3r3b3l@proton.me>
2024-09-08 14:24:17 -0500

    notify now notifies good data

--- scripts/get_ip.sh
+++ scripts/get_ip.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-curl ipinfo.io | jq -r '[.city, .ip] | @csv'  | tr -d '"' | tr ",", " " | tee -a ~/.iplogs.txt | /home/r/.pdtm/go/bin/notify -silent
+local_ip=$(ifconfig wlan0  | head -n 2 | tail -n 1 | cut -d " " -f 10-11 | tr -d " ")
+curl ipinfo.io | jq -r '[.city, .ip] | @csv' | tr -d '"' | tr "," " "| while read city ip; do
+  echo "$(cat /etc/hostname) | $city | $local_ip | $ip" | tee -a ~/.iplogs.txt
+done | /home/r/.pdtm/go/bin/notify -silent

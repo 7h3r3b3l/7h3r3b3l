1f949a0f50727d2b2741322d2bcb0f1ca86b2b75
7h3r3b3l <7h3r3b3l@proton.me>
2024-09-08 14:01:38 -0500

    get ip now notify ip

--- scripts/get_ip.sh
+++ scripts/get_ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl ipinfo.io | jq -r '[.city, .ip] | @csv'  | tr -d '"' | tr ",", " " | tee -a ~/.iplogs.txt
+curl ipinfo.io | jq -r '[.city, .ip] | @csv'  | tr -d '"' | tr ",", " " | tee -a ~/.iplogs.txt | /home/r/.pdtm/go/bin/notify -silent
